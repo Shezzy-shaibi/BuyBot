46ba5cdf3058493fe8df55ebc1a1265c0b5976bb
shezzy-shaibi <shaibimaibi501@gmail.com>
2021-09-17 21:38:24 +0500

    static

--- Myantrabot/settings.py
+++ Myantrabot/settings.py
@@ -74,15 +74,26 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Myantrabot.wsgi.application'
 
 
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'switch2voip',
+#         'USER': 'postgres',
+#         'PASSWORD': 'bitf18a532',
+#
+#     }
+# }
+#
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+    }
+}
 import dj_database_url
 
 db_from_env = dj_database_url.config()
